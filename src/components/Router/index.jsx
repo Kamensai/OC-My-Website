e5e3daeba604f7ce29324680fa055747197d6653
Saïsana <khamvongsa.v@hotmail.fr>
2025-09-06 18:05:42 +0200

    Router: Ajout de la page d'erreur.

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import RouterLayout from '../../layouts/RouteLayout/index.jsx';
 import Home from '../../pages/Home';
 import MyAdventure from '../../pages/MyAdventure/index.jsx';
+import Error from '../../pages/Error';
 
 // Composant AppRouter : définit toutes les routes de l'application avec leur layout principal
 
@@ -11,6 +12,9 @@ function AppRouter() {
       <Route path="/" element={<RouterLayout />}>
         <Route index element={<Home />} />
         <Route path="/my-adventure" element={<MyAdventure />} />
+
+        {/* Page d'erreur pour toute route non trouvée */}
+        <Route path="*" element={<Error />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+function Error() {
+  return (
+    <section className="error-wrapper">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </section>
+  );
+}
+
+export default Error;
