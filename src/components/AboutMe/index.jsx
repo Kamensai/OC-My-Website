b7746164bb1673681155f98d1cda7b9bc229363a
Saïsana <khamvongsa.v@hotmail.fr>
2025-09-07 13:22:47 +0200

    Section A propos terminée.

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,7 +1,19 @@
 function AboutMe() {
   return (
-    <section id="about-me">
+    <section id="about-me" className="about-me">
       <h1>À propos</h1>
+      <p>
+        Désireux d'apprendre, et conscient de l'utilité des site web
+        aujourd'hui, je souhaite mettre mes connaissances à profit pour
+        améliorer le quotidien des utilisateurs.
+        <br />
+        <br />
+        J'ai à cœur de créer des applications permettant notamment de soutenir
+        le secteur du social et médico-social.
+        <br />
+        <br />
+        ⚽ J'aime le sport et le jeux d'énigmes. 🃏 <br />
+      </p>
     </section>
   );
 }
